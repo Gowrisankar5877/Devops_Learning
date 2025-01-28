393878be1e4dedeb48207d16274878a415f0fc41
Gowrisankar <95461759+Gowrisankar5877@users.noreply.github.com>
2025-01-28 15:01:16 +0530

    Update 006_variable.sh

--- ShellScripting/006_variable.sh
+++ ShellScripting/006_variable.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 #Author: Gowri Sankar Marepalli
-#Purpuse: Reading input from the table
+#Purpuse: Checking whether file exists with multiple spaces in file name and without spaces in file name
 #usage: sh filename.sh or ./filename.sh 
 
 file=$1
+if [ -f "$file" ]; then 
+	echo "file exist $file"
+else
+	echo "file doesnot exist"
+fi
 if [ -f $file ]; then 
 	echo "file exist $file"
 else
 	echo "file doesnot exist"
 fi
+
 
