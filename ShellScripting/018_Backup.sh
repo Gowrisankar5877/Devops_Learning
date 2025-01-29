0adb2e65fbdd58b623811cae6e3c59672be3f7b1
Gowrisankar <95461759+Gowrisankar5877@users.noreply.github.com>
2025-01-29 12:32:54 +0530

    backup with error Handling

---
+++ ShellScripting/018_Backup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#Author: Gowri Sankar Marepalli
+#Purpose: Getting backup
+#usage: sh filename.sh or ./filename.sh
+
+function backup {
+    echo "Enter the file name to take backup"
+    read -r backupfile
+    if [ -f $backupfile ]; then
+        echo "File exists"
+        cp $backupfile /tmp/backupfile
+        echo "Backup is taken"
+    else
+        echo "File does not exist"
+    fi
+    cp $backupfile /tmp/backupfile
+    echo $?
+    if [ $? -ne 0 ]; then #ne is not equal to
+        echo "Backup failed $?"
+    fi
+}
+backup
