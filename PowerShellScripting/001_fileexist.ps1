de8421e7648f6f20d514c6cabbc090c016a425e5
Gowrisankar <95461759+Gowrisankar5877@users.noreply.github.com>
2025-01-31 14:51:49 +0530

    File Existence and listing files

---
+++ PowerShellScripting/001_fileexist.ps1
@@ -0,0 +1,14 @@
+#power shell script to check whether the desktop folder exists or not and listing files
+
+$folderpath = Read-Host "Enter the folder path"
+if( Test-Path $folderpath)
+{
+    Write-Host "Folder exists"
+    cd $folderpath
+    echo "Files in the folder are:"
+    ls
+}
+else
+{
+    Write-Host "Folder does not exist"
+}
