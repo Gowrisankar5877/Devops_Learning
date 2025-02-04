8ea9077d0d9c9b40107fefc7f71604a516e48dc3
Gowrisankar5877 <mgowrisankar5877@gmail.com>
2025-02-04 12:20:40 +0530

    learning Strings

---
+++ python/002_String.py
@@ -0,0 +1,9 @@
+multi_line_string = '''I am Devops Engineer at UST. 
+I am looking forward to grow in the organization in 5 years and experience different roles '''
+print(multi_line_string)
+Fname = "Gowri Sankar"
+Lname = "Marepalli"
+print("name is", Fname+" "+Lname)
+print(len(Fname+" "+Lname))
+string="I am Devops Engineer at UST. \n I am looking forward to grow in the organization in 5 years and experience different roles. \tEnjoy Life"
+print(string)
