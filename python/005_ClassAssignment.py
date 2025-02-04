125782aa1f1d75d035448d1f4aa0a316d8e52562
Gowrisankar5877 <mgowrisankar5877@gmail.com>
2025-02-04 16:57:09 +0530

    String tasks

---
+++ python/005_ClassAssignment.py
@@ -0,0 +1,14 @@
+#concatenate the string devops batch 2
+str1 = "Devops"
+str2 = "batch"
+str3 = "2"
+string = str1+" "+str2+" "+str3
+print(string)
+print(len(string))
+print((string.lower()).find("b"))
+print(string[:len(str1)])
+print(string.split()[0])
+print(string[-1])
+print(len(string)-1)
+print(string.index(string[-1]))
+print(string.index("b"))
