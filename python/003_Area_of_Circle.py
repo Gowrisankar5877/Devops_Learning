9e6bad129fc7cc1a499d9fc8758ea61acb349419
Gowrisankar5877 <mgowrisankar5877@gmail.com>
2025-02-04 14:00:45 +0530

    adding multiplication table and area of circle

---
+++ python/003_Area_of_Circle.py
@@ -0,0 +1,4 @@
+#Area of a circle
+pi = 22/7
+r = int(input("Enter radius of the circle"))
+print("Area of circle", pi*r**2)
---
+++ python/004_Multiplication_Table.py
@@ -0,0 +1,4 @@
+#Multiplication Table
+n = int(input("Enter the number:"))
+for i in range(1,11):
+    print(str(n)+"*"+str(i)+"="+str(n*i))
