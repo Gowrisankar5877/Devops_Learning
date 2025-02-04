010e3ddeab5658139e78bdb7f3b4101b3911037e
Gowrisankar5877 <mgowrisankar5877@gmail.com>
2025-02-04 12:08:27 +0530

    basics of python

---
+++ python/001_basic.py
@@ -0,0 +1,20 @@
+Fname = "Gowri Sankar"
+Lname = "Marepalli"
+Age = 22
+PerInfo = {'Fname':"Gowri Sankar", 'Lname':"Marepalli", 'Age':22}
+print(PerInfo)
+print(len(PerInfo))
+x=5
+x**=3
+print(x)
+x//=5
+print(x)
+x&=3
+print(x)
+length=12
+width=10
+area=length*width
+print(area)
+print(3>=2 and 4>=2) 
+print(3>=2 or 4>=1 )
+print(not(3>=2))
