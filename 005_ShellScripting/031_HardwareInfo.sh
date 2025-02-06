99f887e4207c09f0c62291ff2cd46b7a11cad4fc
Gowrisankar5877 <mgowrisankar5877@gmail.com>
2025-02-06 09:56:53 +0530

    Updating files

---
+++ 005_ShellScripting/031_HardwareInfo.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#Author: Gowri Sankar Marepalli
+#Purpose: Knowing the hardware info of the system
+#usage: ./Filename.sh or sh Filename.sh
+
+echo "Hardware Info"
+lscpu
+echo "---------------------------------"
+echo "---------------------------------"
+echo "Hardware Info through lshw"
+lshw
+
