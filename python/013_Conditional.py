22b4b6e3170aba06246da2fe6dd22336d5f70291
Gowrisankar5877 <mgowrisankar5877@gmail.com>
2025-02-08 10:49:37 +0530

    Updating coditional and While files

---
+++ python/013_Conditional.py
@@ -0,0 +1,7 @@
+num = int(input("Enter a number"))
+if num > 10:
+    print("Number is greater than 10")
+elif num < 5:
+    print("Number is lessthan 5")
+else:
+    print("Number out of range")
---
+++ python/014_While.py
@@ -0,0 +1,7 @@
+num = int(input("Enter a number"))
+i=1
+while i<11:
+    print(str(num)+"*"+str(i)+"="+str(num*i))
+    i+=1
+else:
+    print("Table printing done")
